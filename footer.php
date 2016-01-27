73a93ab1266bc8d4d46fbfc3d3c977ff7f98515a
Yuriy Kurenkov <kurenkov@ukr.net>
2016-01-27 11:05:18 +0200

    adding YM counter to application

--- footer.php
+++ footer.php
@@ -44,5 +44,35 @@
   </div>
 </div>
 
+<!-- YM counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter34945715 = new Ya.Metrika({
+                    id:34945715,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/34945715" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /YM counter -->
+
 </body>
 </html>
